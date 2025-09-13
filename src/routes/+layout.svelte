dc46677c56ee4e1cf431624b2b90fce6cea5ae9f
Unrealrojo234 <otienoryan812@gmail.com>
2025-09-13 04:38:05 +0300

    can now successfully build

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+
+
 </script>
 
 <svelte:head>
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,15 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node'; // or adapter-auto
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() }, strict: false };
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter(),
+		prerender: {
+			handleUnseenRoutes: 'warn' // or 'ignore' instead of throwing an error
+		}
+	}
+};
 
 export default config;
