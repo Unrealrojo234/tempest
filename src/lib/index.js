c937d16da81ed82fb27aa90ae1827a2db872eb33
Unrealrojo234 <otienoryan812@gmail.com>
2025-09-16 15:02:45 +0300

    done

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,6 @@
 import PocketBase from 'pocketbase';
-import { env } from '$env/dynamic/public';
 
-const pbUrl = env.PUBLIC_POCKETBASE_URL || 'http://localhost:8090';
+const pbUrl =  'http://localhost:8090';
 
 const pb = new PocketBase(pbUrl);
 
