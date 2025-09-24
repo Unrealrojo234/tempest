29bfa5f2409b1f2a032dd12adb8133ba8eb98f99
Unrealrojo234 <otienoryan812@gmail.com>
2025-09-24 20:16:23 +0300

    nuance fixes

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,7 @@
 import PocketBase from 'pocketbase';
+import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
 
-const pbUrl =  'http://localhost:8090';
+const pbUrl = PUBLIC_POCKETBASE_URL ;
 
 const pb = new PocketBase(pbUrl);
 
